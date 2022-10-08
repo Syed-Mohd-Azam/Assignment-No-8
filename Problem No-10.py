c4c850d74219e9a81956d0367d23d7dacdfd9c0f
SYED MOHD AZAM <112909412+Syed-Mohd-Azam@users.noreply.github.com>
2022-10-08 22:40:45 +0530

    Add files via upload

---
+++ Problem No-10.py
@@ -0,0 +1,6 @@
+# Write a python script to print first 10 multiples of 5.
+i=1
+while(i<=10):
+    print((5*i),end=" ")
+    i=i+1
+print()
---
+++ Problem No-4.py
@@ -0,0 +1,6 @@
+# Write a python script to print first 10 odd natural numbers.
+i=0
+while(i<=9):
+    print((2*i+1),end=" ")
+    i=i+1
+print()
